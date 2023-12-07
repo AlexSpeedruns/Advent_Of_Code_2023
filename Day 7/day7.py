710e6d80558ce9306cf648ea22e6589be9473f10
AlexSpeedruns <44741588+AlexSpeedruns@users.noreply.github.com>
2023-12-07 01:45:27 -0500

    Day 7

--- Day 7/day7.py
+++ Day 7/day7.py
@@ -19,31 +19,7 @@ def part1(lines):
         updated_lines.append((line.split()[0], line.split()[1]))
 
     for cards, bid in updated_lines:
-        counter = Counter([card for card in cards])
-        sorted_cards = list(sorted(counter.values()))
-        score = -1
-        type = -1
-
-        if sorted_cards == [5]:
-            type = 6
-        elif sorted_cards == [1, 4]:
-            type = 5
-        elif sorted_cards == [2, 3]:
-            type = 4
-        elif sorted_cards == [1, 1, 3]:
-            type = 3
-        elif sorted_cards == [1, 2, 2]:
-            type = 2
-        elif sorted_cards == [1, 1, 1, 2]:
-            type = 1
-        elif sorted_cards == [1, 1, 1, 1, 1]:
-            type = 0
-
-        for i, c in enumerate(cards, start=1):
-            score += (len(card_value_map) ** (len(cards) - i)) * card_value_map[c]
-
-        score += (len(card_value_map) ** len(cards)) * type
-
+        score = calculate_score(cards, card_value_map)
         points.append((score, int(bid)))
 
     points = sorted(points, key=operator.itemgetter(0))
@@ -67,31 +43,7 @@ def part2(lines):
         highest_score = -1
         for card in cards:
             updated_cards = cards.replace("J", card)
-
-            counter = Counter([card for card in updated_cards])
-            sorted_cards = list(sorted(counter.values()))
-            score = -1
-            type = -1
-
-            if sorted_cards == [5]:
-                type = 6
-            elif sorted_cards == [1, 4]:
-                type = 5
-            elif sorted_cards == [2, 3]:
-                type = 4
-            elif sorted_cards == [1, 1, 3]:
-                type = 3
-            elif sorted_cards == [1, 2, 2]:
-                type = 2
-            elif sorted_cards == [1, 1, 1, 2]:
-                type = 1
-            elif sorted_cards == [1, 1, 1, 1, 1]:
-                type = 0
-
-            for i, c in enumerate(cards, start=1):
-                score += (len(card_value_map) ** (len(cards) - i)) * card_value_map[c]
-
-            score += (len(card_value_map) ** len(cards)) * type
+            score = calculate_score(cards, card_value_map, updated_cards)
             if score > highest_score:
                 highest_score = score
         points.append((highest_score, int(bid)))
@@ -104,6 +56,36 @@ def part2(lines):
         total += (i+1) * bid
 
     print(total)
+
+def calculate_score(cards, card_value_map, updated_cards=[]):
+    if updated_cards:
+        counter = Counter([card for card in updated_cards])
+    else:
+        counter = Counter([card for card in cards])
+    sorted_cards = list(sorted(counter.values()))
+    score = -1
+    type = -1
+
+    if sorted_cards == [5]:
+        type = 6
+    elif sorted_cards == [1, 4]:
+        type = 5
+    elif sorted_cards == [2, 3]:
+        type = 4
+    elif sorted_cards == [1, 1, 3]:
+        type = 3
+    elif sorted_cards == [1, 2, 2]:
+        type = 2
+    elif sorted_cards == [1, 1, 1, 2]:
+        type = 1
+    elif sorted_cards == [1, 1, 1, 1, 1]:
+        type = 0
+
+    for i, c in enumerate(cards, start=1):
+        score += (len(card_value_map) ** (len(cards) - i)) * card_value_map[c]
+
+    score += (len(card_value_map) ** len(cards)) * type
+    return score
 
 if __name__ == '__main__':
     main()
